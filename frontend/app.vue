ef4ea5832724b89da34b57c5b830a63e481537a8
Caio Costa <caiosouzacosta@santos.sp.gov.br>
2024-04-22 11:34:32 -0300

    Meta tags

--- frontend/app.vue
+++ frontend/app.vue
@@ -11,6 +11,21 @@ useHead({
       ? `${titleChunk} - Caio Costa's Portfolio`
       : "Caio Costa | Caio Dev | Fullstack Developer";
   },
+  meta: [
+    {
+      name: "description",
+      content: "My new, enhanced and dyanamic portfolio.",
+    },
+    {
+      name: "keywords",
+      content:
+        "Fullstack Developer, Back-end, Front-end, Hard Skills, Soft Skills, HTML, CSS, JavaScript, TypeScript, Vue.js, Nuxt.js, PHP, Laravel, MySQL, Microsoft Windows, Linux",
+    },
+    {
+      name: "autor",
+      content: "Caio Costa",
+    },
+  ],
 });
 </script>
 
